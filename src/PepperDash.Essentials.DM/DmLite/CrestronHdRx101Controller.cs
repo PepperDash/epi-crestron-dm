5a5914882581d689a4734ed20014e0bddea68dbd
Nick Genovese <ngenovese@pepperdash.com>
2023-01-20 13:32:36 -0800

    feature: added in dm lite receiver

--- src/PepperDash.Essentials.DM/DmLite/CrestronHdRx101Controller.cs
+++ src/PepperDash.Essentials.DM/DmLite/CrestronHdRx101Controller.cs
@@ -1,12 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Crestron.SimplSharp;
+using Crestron.SimplSharpPro.DeviceSupport;
+using Crestron.SimplSharpPro.DM.Endpoints.Receivers;
+using PepperDash.Essentials.Core;
+using PepperDash.Essentials.Core.Bridges;
 
 namespace PepperDash.Essentials.DM.DmLite
 {
-    public class CrestronHdRxc101Controller
+    public class CrestronHdRx101Controller : EssentialsBridgeableDevice,
+                                             ICommunicationMonitor,
+                                             IOnline,
+                                             IRoutingInputsOutputs
+
     {
+        private HdRx101CEDmLite _rx;
+        private CrestronGenericBaseCommunicationMonitor _monitor;
+
+        public CrestronHdRx101Controller(string key, string name, Func<HdRx101CEDmLite> builder) : base(key, name)
+        {
+            InputPorts = new RoutingPortCollection<RoutingInputPort>
+                         {
+                             new RoutingInputPort("DmIn",
+                                                  eRoutingSignalType.AudioVideo,
+                                                  eRoutingPortConnectionType.DmCat,
+                                                  new object(),
+                                                  this)
+                         };
+
+            OutputPorts = new RoutingPortCollection<RoutingOutputPort>
+                          {
+                              new RoutingOutputPort("HdmiOut",
+                                                    eRoutingSignalType.AudioVideo,
+                                                    eRoutingPortConnectionType.Hdmi,
+                                                    new object(),
+                                                    this)
+                          };
+
+            AddPreActivationAction(() => _rx = builder());
+            AddPreActivationAction(() => _monitor = new CrestronGenericBaseCommunicationMonitor(this, _rx, 30000, 60000));
+        }
+
+        public override void Initialize()
+        {
+            if (_rx.Registerable)
+            {
+                _rx.RegisterWithLogging(Key);
+            }
+        }
+
+        public override void LinkToApi(BasicTriList trilist, uint joinStart, string joinMapKey, EiscApiAdvanced bridge)
+        {
+           
+            
+        }
+
+        public StatusMonitorBase CommunicationMonitor
+        {
+            get { return _monitor; }
+        }
+
+        public BoolFeedback IsOnline
+        {
+            get { return _monitor.IsOnlineFeedback; }
+        }
+
+        public RoutingPortCollection<RoutingInputPort> InputPorts { get; private set; }
+        public RoutingPortCollection<RoutingOutputPort> OutputPorts { get; private set; }
     }
 }
