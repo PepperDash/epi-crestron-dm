829b52feaf5af4e110bffec7e9d151aa14ae581f
Nick Genovese <ngenovese@pepperdash.com>
2023-01-20 13:32:11 -0800

    feature: initial repo setup

---
+++ src/PepperDash.Essentials.DM/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("PepperDash.Essentials.DM")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PepperDash.Essentials.DM")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
